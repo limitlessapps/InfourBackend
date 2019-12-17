7cab286dfaa342130ee6bbee347f68a4e2ae1fee
zachee <zacheeishimwe@gmail.com>
2019-12-17 14:28:25 +0200

    fixing update account

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -67,50 +67,104 @@ exports.get_account = (req,res,next)=>{
 }
 
 
+// exports.modify_account = (req,res,next)=>{
+//     const account = new Account({
+//         _id:req.params.id,
+//         first_name:req.body.first_name,
+//         middle_name:req.body.middle_name,
+//         surname:req.body.surname,
+//         date_of_birth:req.body.date_of_birth,
+//         place_of_birth:req.body.place_of_birth,
+//         sex:req.body.sex,
+//         nationality:req.body.nationality,
+//         id_number:req.body.id_number,
+//         martial_status:req.body.martial_status,
+//         country:req.body.country,
+//         province:req.body.province,
+//         district:req.body.district,
+//         cell:req.body.cell,
+//         village:req.body.village,
+//         email:req.body.email,
+//         email_work:req.body.email_work,
+//         primary_number:req.body.primary_number,
+//         secondary_number:req.body.secondary_number,
+//         family:req.body.family,
+//         occupation:req.body.occupation,
+//         insurance:req.body.insurance,
+//         bank:req.body.bank,
+//         assets:req.body.assets,
+//         social_media:req.body.social_media,
+//         file_upload:req.body.file_upload,
+//         relative:req.body.relative,
+//         });
+//          Account.updateOne({_id:req.params.id,account})
+//          .then(result=>{
+//              res.status(200).json({
+//                  result:result,
+//                  consoleResult:console.log(result)
+//              })
+//          })
+//          .catch(err=>{
+//              res.status(400).json({
+//                  error:err
+//              })
+//          })
+// }
+
+
+
+
+
 
 exports.modify_account = (req,res,next)=>{
-    const account = new Account({
-        _id:req.params.id,
-        first_name:req.body.first_name,
-        middle_name:req.body.middle_name,
-        surname:req.body.surname,
-        date_of_birth:req.body.date_of_birth,
-        place_of_birth:req.body.place_of_birth,
-        sex:req.body.sex,
-        nationality:req.body.nationality,
-        id_number:req.body.id_number,
-        martial_status:req.body.martial_status,
-        country:req.body.country,
-        province:req.body.province,
-        district:req.body.district,
-        cell:req.body.cell,
-        village:req.body.village,
-        email:req.body.email,
-        email_work:req.body.email_work,
-        primary_number:req.body.primary_number,
-        secondary_number:req.body.secondary_number,
-        family:req.body.family,
-        occupation:req.body.occupation,
-        insurance:req.body.insurance,
-        bank:req.body.bank,
-        assets:req.body.assets,
-        social_media:req.body.social_media,
-        file_upload:req.body.file_upload,
-        relative:req.body.relative,
-        });
-         Account.updateOne({_id:req.params.id,account})
-         .then(result=>{
-             res.status(200).json({
-                 result:result,
-                 consoleResult:console.log(result)
-             })
-         })
-         .catch(err=>{
-             res.status(400).json({
-                 error:err
-             })
-         })
+   const body = {
+    _id:req.params.id,
+    first_name:req.body.first_name,
+    middle_name:req.body.middle_name,
+    surname:req.body.surname,
+    date_of_birth:req.body.date_of_birth,
+    place_of_birth:req.body.place_of_birth,
+    sex:req.body.sex,
+    nationality:req.body.nationality,
+    id_number:req.body.id_number,
+    martial_status:req.body.martial_status,
+    country:req.body.country,
+    province:req.body.province,
+    district:req.body.district,
+    cell:req.body.cell,
+    village:req.body.village,
+    email:req.body.email,
+    email_work:req.body.email_work,
+    primary_number:req.body.primary_number,
+    secondary_number:req.body.secondary_number,
+    family:req.body.family,
+    occupation:req.body.occupation,
+    insurance:req.body.insurance,
+    bank:req.body.bank,
+    assets:req.body.assets,
+    social_media:req.body.social_media,
+    file_upload:req.body.file_upload,
+    relative:req.body.relative
+   }
+   console.log(body)
+   let _id = req.params.id;
+   Account.findOneAndUpdate({ _id }, body)
+   .then(result=>{
+    res.status(200).json(result)
+})
+.catch(err=>{
+    res.status(400).json({
+        error:err
+    })
+})
 }
+
+
+
+
+
+
+
 
 
 
@@ -129,3 +183,4 @@ exports.delete_account = (req,res,next)=>{
       }
   )
   }
+
